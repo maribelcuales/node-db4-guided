93405d981022e628531f7a94bdf31390efc2f105
maribelcuales <maribelcuales@gmail.com>
2020-09-12 20:20:11 +0800

    Created queries.sql for query commands.

---
+++ queries.sql
@@ -0,0 +1,7 @@
+select * 
+from orders as o
+join customers as c on o.customer_id = c.customer_id
+join employees as e on o.employee_id = e.employee_id
+join shippers as s on o.ship_via = s.shipper_id
+join order_details as od on o.order_id = od.order_id
+join products as p on od.product_id = p.product_id;
